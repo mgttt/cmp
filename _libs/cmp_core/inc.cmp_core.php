faafa840b607be012f1575b2e2885205c1c716c9
wanjochan <wanjochan@gmail.com>
2015-07-08 08:19:39 +0800

    rb4.2.3_cmp.php

--- _libs/cmp_core/inc.cmp_core.php
+++ _libs/cmp_core/inc.cmp_core.php
@@ -1,23 +1,22 @@
 <?php
-/**
- ** 在引用位置使用.
- if(!defined("_APP_DIR_")) define("_APP_DIR_",realpath(dirname(__FILE__)));
+
+# Don't Modify Me, You can copy and run at your own DIR, like:
+#_APP_DIR_ .'/inc.app.php':
+/*
+if (!defined("_APP_DIR_")) define("_APP_DIR_",realpath(dirname(__FILE__)));
+#if (!defined("_LIB_")) define("_LIB_",realpath(__DIR__ ."/_libs/"));
+require '../_libs/cmp_core/inc.cmp_core.php';
  */
-//基本够用，如果不够，复制一份并放在目录微调一下就可以了.
 
 error_reporting(E_ERROR|E_COMPILE_ERROR|E_PARSE|E_CORE_ERROR|E_USER_ERROR);
 
-if (!defined("_APP_DIR_"))
-	//define("_APP_DIR_",realpath(dirname(__FILE__)));
-	define("_APP_DIR_",realpath(__DIR__ ."/../../"));
+if (!defined("_APP_DIR_")) define("_APP_DIR_",realpath(__DIR__ ."/../../"));
 
-if (!defined("_LIB_"))
-	define("_LIB_",realpath(__DIR__ ."/../"));
+if (!defined("_LIB_")) define("_LIB_",realpath(__DIR__ ."/../"));
 
-if (_LIB_=="" || _LIB_=="_LIB_")
-	throw new Exception("empty _LIB_");
+if (_LIB_=="" || _LIB_=="_LIB_") throw new Exception("empty _LIB_");
 
-require_once 'inc.header.sample.2014b.php';//if not enough, copy it and make your own.
+require_once 'inc.header.sample.2014b.php';//Again, if not enough, copy it and make your own.
 
 //adjust_timezone();//adjust php timezone getConf(SERVER_TIMEZONE), ignore it if using db time
 
