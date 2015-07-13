7021c168b8ee0f051146b299a9f4dcf9e9c4380a
wanjochan <wanjochan@gmail.com>
2015-07-13 16:52:53 +0800

    更新demo

--- cmp_demo/ApiDemo.php
+++ cmp_demo/ApiDemo.php
@@ -18,4 +18,56 @@ class ApiDemo
 		}
 		return $rt;
 	}
+	//{ "errmsg": "Maximum execution time of 30 seconds exceeded", "errno": 1, "line": 24, "module": "ApiDemo" }
+	public function TestBlackHole1($param){
+		sleep(31);
+	}
+	public function TestBlackHole2($param){
+	}
+	public function SaveTestNote($param){
+		if($param['id']){
+			return self::UpdateTestNote($param);
+		}else{
+			return self::AddTestNote($param);
+		}
+	}
+	public function AddTestNote($param){
+		//TODO 要check必要的输入
+
+		$param['note_status'] = "0";
+		//访问orm层
+		$cls = new OrmDemo(LgcDemo::getDSN());
+		$info = $cls->newOrm($param);
+		if($info){
+			return array("STS"=>"OK","data"=>$info);
+		}
+		return array("STS"=>"KO");
+	}
+	public function UpdateTestNote($param){
+		//TODO 要check必要的输入
+
+		//访问orm层
+		$cls = new OrmDemo(LgcDemo::getDSN());
+		$info = $cls->updateOrm($param['id'],$param);
+		if($info){
+			return array("STS"=>"OK","data"=>$info);
+		}
+		return array("STS"=>"KO");
+	}
+
+	public function DeleteTestNote($param){
+		//TODO 要check必要的输入
+
+		//访问orm层
+		$cls = new OrmDemo(LgcDemo::getDSN());
+		$info = $cls->deleteBean($param['id']);
+		if($info){
+			return array("STS"=>"OK","data"=>$info);
+		}
+		return array("STS"=>"KO");
+	}
+	public function ListTestNote($param){
+		$cls = new LgcDemo();
+		return $cls->ListTestNote($param);
+	}
 }
--- cmp_demo/_conf.cmp_demo/cmp_demo.php
+++ cmp_demo/_conf.cmp_demo/cmp_demo.php
@@ -8,3 +8,8 @@ $_conf_all_[$_switch_conf]=$_conf_all_common_;//don't delete this one...
 #	"*"=>1,//1 to open debug all for quicklog(); but quicklog_must() bypass this check
 #);
 
+$_conf_all_[$_switch_conf]["flag_rb_freeze"]=false;//在DEMO不为FREEZE表结构，这样在首次运行都能运行.
+
+#在代码中
+#setConf("flag_rb_freeze",false);//DEMO不需要freeze
+
