d9d4337edfd74f69f7cc623949ea4de143878e28
wanjochan <wanjochan@gmail.com>
2016-05-29 19:59:38 +0800

    demo 1.X

--- cmp_demo_1/WebCmpDemo1.php
+++ cmp_demo_1/WebCmpDemo1.php
@@ -4,4 +4,14 @@ class WebCmpDemo1
 	public function DefaultIndex(){
 		include(CmpService::TPL("DefaultIndex"));
 	}
+
+	public function Demo1_1(){
+		include(CmpService::TPL("demo1.1"));
+	}
+	public function Demo1_2(){
+		include(CmpService::TPL("demo1.2"));
+	}
+	public function Demo1_3(){
+		include(CmpService::TPL("demo1.3"));
+	}
 }
