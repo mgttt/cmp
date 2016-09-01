00c46b914026317f5ebcee8c9af4ebc9f2afd13f
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-09-01 11:43:53 +0800

    Update server_start.sh

--- server_start.sh
+++ server_start.sh
@@ -11,6 +11,7 @@ dd=$(pwd)
 
 #echo docker pull cmptech/cmp_app_server
 
-#docker run --name $(date +%Y%m%d%H%M%S) -p ${slc_port}:9501 -v $dd/app_root:/app_root -w /root/ cmptech/cmp_app_server sh start_cmp_server_docker_local.sh
-echo $dd
-docker run --name $(date +%Y%m%d%H%M%S) -p ${slc_port}:9501 -w /root/ cmptech/cmp_app_server sh start_cmp_server_docker_local.sh
+docker run --name $(date +%Y%m%d%H%M%S) -p ${slc_port}:9501 -v $dd/app_root:/app_root -w /root/ cmptech/cmp_app_server sh start_cmp_server_docker_local.sh
+
+#echo $dd
+#docker run --name $(date +%Y%m%d%H%M%S) -p ${slc_port}:9501 -w /root/ cmptech/cmp_app_server sh start_cmp_server_docker_local.sh
